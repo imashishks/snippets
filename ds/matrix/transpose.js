21444e692af3ec463a88d304f57707a0d1aaa2fc
imashishks <sayhitoak@gmail.com>
2021-07-06 23:04:53 +0530

    Transpose of matrix added

---
+++ ds/matrix/transpose.js
@@ -0,0 +1,18 @@
+const arr = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
+];
+
+const transpose = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let temp = arr[i][j];
+      arr[i][j] = arr[j][i];
+      arr[j][i] = temp;
+    }
+  }
+  return arr;
+};
+console.log(transpose(arr));
