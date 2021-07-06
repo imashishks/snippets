1d21a1cdd84adc2a7bc5cd6bd61d83dcd7c919d0
imashishks <sayhitoak@gmail.com>
2021-07-06 22:28:21 +0530

    snake pattern added

--- ds/matrix/snake.js
+++ ds/matrix/snake.js
@@ -3,9 +3,26 @@
 
 */
 var arr = [
-    [0,1,2,3],
-    [4,5,6,7],
-    [8,9,10,11],
-    [12,13,14,15],
-    [16,17,18,19]
-    ];
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
+  [16, 17, 18, 19],
+];
+
+const getSnake = (arr) => {
+  const tempArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 == 0) {
+      for (let j = 0; j < arr[i].length; j++) {
+        tempArr.push(arr[i][j]);
+      }
+    } else {
+      for (let j = arr[i].length - 1; j >= 0; j--) {
+        tempArr.push(arr[i][j]);
+      }
+    }
+  }
+  return tempArr;
+};
+console.log(getSnake(arr));
