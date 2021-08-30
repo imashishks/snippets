295e0a80d1d6d722284341a12452cf8d7e636d91
Ashish Kumar <sayhitoak@gmail.com>
2021-08-30 23:25:04 +0530

    tree wip

---
+++ ds/trees/tree.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
